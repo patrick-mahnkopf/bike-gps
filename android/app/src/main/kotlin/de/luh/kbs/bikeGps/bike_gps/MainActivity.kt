5299b5d5fece1e8efa643778195531807755d861
Patrick Mahnkopf <mahnkopf@stud.uni-hannover.de>
2021-03-09 23:54:31 +0100

    Added Android file type associations to allow 'open with' functionality

--- android/app/src/main/kotlin/de/luh/kbs/bikeGps/bike_gps/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package de.luh.kbs.bikeGps.bike_gps
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/de/luh/kbs/bike_gps/MainActivity.kt
+++ android/app/src/main/kotlin/de/luh/kbs/bike_gps/MainActivity.kt
@@ -1,6 +1,18 @@
 package de.luh.kbs.bike_gps
 
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+      if (intent.getIntExtra("org.chromium.chrome.extra.TASK_ID", -1) == this.taskId) {
+          this.finish();
+          intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+          startActivity(intent);
+      }
+      super.onCreate(savedInstanceState);
+      handleIntent();
+  }
 }
