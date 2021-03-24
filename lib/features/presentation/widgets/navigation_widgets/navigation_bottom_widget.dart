33c571faba0014e532ae9772bf79834d410b4b3a
Patrick Mahnkopf <mahnkopf@stud.uni-hannover.de>
2021-03-24 11:05:01 +0100

    Now clearing path to tour when navigation is stopped

--- lib/features/presentation/widgets/navigation_widgets/navigation_bottom_widget.dart
+++ lib/features/presentation/widgets/navigation_widgets/navigation_bottom_widget.dart
@@ -112,6 +112,7 @@ class GrabSectionContent extends StatelessWidget {
     if (mapboxState is MapboxLoadSuccess && tourState is TourLoadSuccess) {
       mapboxState.controller.animateCameraToTourBounds(
           tour: tourState.tour, alternativeTours: tourState.alternativeTours);
+      mapboxState.controller.clearPathToTour();
       mapboxBloc.add(MapboxLoaded(
           mapboxController: mapboxState.controller,
           myLocationTrackingMode: MyLocationTrackingMode.None));
