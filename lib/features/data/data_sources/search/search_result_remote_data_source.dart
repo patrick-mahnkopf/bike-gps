febafd978b20cd6f1da7097e6b51b1bee07ec3e2
Patrick Mahnkopf <mahnkopf@stud.uni-hannover.de>
2021-03-29 16:52:50 +0200

    Improved search and implemented listing all tours by searching for 'tour' among other alternatives

--- lib/features/data/data_sources/search/search_result_remote_data_source.dart
+++ lib/features/data/data_sources/search/search_result_remote_data_source.dart
@@ -5,6 +5,7 @@ import 'package:bike_gps/features/data/models/search/models.dart';
 import 'package:bike_gps/features/domain/entities/tour/tour_info.dart';
 import 'package:bike_gps/features/domain/usecases/tour/get_tour.dart';
 import 'package:bike_gps/injection_container.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:geocoder/geocoder.dart';
 import 'package:http/http.dart' as http;
@@ -24,34 +25,69 @@ class SearchResultRemoteDataSourceImpl implements SearchResultRemoteDataSource {
 
   @override
   Future<List<SearchResultModel>> getSearchResults({String query}) async {
+    final List<SearchResultModel> searchResults = [];
+    final List<TourInfo> tourInfos = tourListHelper.asList;
+
+    final Tuple2<bool, String> isTourQueryAndSanitizedQuery =
+        _getIsTourQueryAndSanitizedQuery(query);
+    final bool isTourQuery = isTourQueryAndSanitizedQuery.value1;
+    final String sanitizedQuery = isTourQueryAndSanitizedQuery.value2;
+
+    for (final TourInfo tourInfo in tourInfos) {
+      final String tourName = tourInfo.name.toLowerCase();
+      if (tourName.contains(sanitizedQuery)) {
+        final SearchResultModel searchResult =
+            await _getSearchResultModelFromTourInfo(tourInfo);
+        searchResults.add(searchResult);
+      }
+    }
+
+    if (!isTourQuery) {
+      final List<SearchResultModel> geocoderResults =
+          await _getGeocoderResults(query);
+      for (final SearchResultModel geocoderResult in geocoderResults) {
+        searchResults.add(geocoderResult);
+      }
+    }
+
+    return searchResults;
+  }
+
+  Tuple2<bool, String> _getIsTourQueryAndSanitizedQuery(String query) {
+    final String initialQuery = query.trim().toLowerCase();
+    String sanitizedQuery = initialQuery;
+    final List<String> tourQueryStartStrings = [
+      'tours:',
+      'tours',
+      'tour:',
+      'tour'
+    ];
+
+    for (final String tourQueryStartString in tourQueryStartStrings) {
+      if (sanitizedQuery.startsWith(tourQueryStartString)) {
+        sanitizedQuery =
+            sanitizedQuery.replaceFirst(tourQueryStartString, '').trim();
+      }
+    }
+
+    if (initialQuery != sanitizedQuery) {
+      return Tuple2(true, sanitizedQuery);
+    }
+    return Tuple2(false, sanitizedQuery);
+  }
+
+  Future<List<SearchResultModel>> _getGeocoderResults(String query) async {
+    // TODO replace with self hosted alternative
     final response = await http.get('https://photon.komoot.io/api/?q=$query');
     final body = json.decode(utf8.decode(response.bodyBytes));
     final features = body['features'] as List;
 
-    final List<SearchResultModel> searchResults = features
+    return features
         .map((searchResult) => SearchResultModel.fromJson(
             searchResult as Map<String, dynamic>,
             tourListHelper: getIt()))
         .toSet()
         .toList();
-
-    final List<TourInfo> tourInfos = tourListHelper.asList;
-    tourInfos.retainWhere((tourInfo) {
-      final String tourName = tourInfo.name;
-      if (query.length >= (tourName.length / 3)) {
-        return tourName.toLowerCase().contains(query.toLowerCase());
-      } else {
-        return false;
-      }
-    });
-
-    for (final TourInfo tourInfo in tourInfos) {
-      final SearchResultModel searchResult =
-          await _getSearchResultModelFromTourInfo(tourInfo);
-      searchResults.insert(0, searchResult);
-    }
-
-    return searchResults;
   }
 
   Future<SearchResultModel> _getSearchResultModelFromTourInfo(
